f5c9951b157867f9a2c2901bb45a4fe47dc42d8d
scramatte <scramatte@id.nixus.es>
2024-09-21 11:24:25 +0200

    get initials from name

--- resources/js/components/Avatar.vue
+++ resources/js/components/Avatar.vue
@@ -55,7 +55,8 @@ export default {
       if (!this.name || typeof this.name !== "string" || !this.name.length) {
         return "";
       }
-      return this.name.charAt(0);
+
+      return this.name.match(/(^\S\S?|\s\S)?/g).map(v=>v.trim()).join("").match(/(^\S|\S$)?/g).join("").toLocaleUpperCase();
     }
   }
 };
