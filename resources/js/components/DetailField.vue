a47a139fdd33d45c6ea47d5ac206276b39552fce
scramatte <scramatte@id.nixus.es>
2024-09-21 10:33:39 +0200

    Add vue-avatar initial package

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,19 @@
 <template>
-  <PanelItem :index="index" :field="field" />
+  <vue-avatar :username="fieldValue" />
 </template>
 
 <script>
+import VueAvatar from "@webzlodimir/vue-avatar";
+
 export default {
   props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+  components: {
+    VueAvatar
+  },
+  computed: {
+    fieldValue() {
+      return this.field.displayedAs || this.field.value
+    },
+  }
 }
 </script>
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,11 +1,15 @@
 <template>
-  <span>{{ fieldValue }}</span>
+  <vue-avatar :username="fieldValue" />
 </template>
 
 <script>
+import VueAvatar from "@webzlodimir/vue-avatar";
+
 export default {
   props: ['resourceName', 'field'],
-
+  components: {
+    VueAvatar
+  },
   computed: {
     fieldValue() {
       return this.field.displayedAs || this.field.value
